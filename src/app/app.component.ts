603b4712ed5cffc1584c55e2b638b4120f40d026
Juanpablo068 <juanpagil9@gmail.com>
2024-03-05 16:13:02 -0500

    Actualizacion Front

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { UserStorageService } from './services/storage/user-storage.service';
+
+
+interface SidenavToggle{
+  screenWidth: number;
+  collapsed: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Mateos Tienda';
+ 
+ isSideNavCollapsed = false;
+  screenWidth = 0;
+
+  //logout() {
+  //UserStorageService.signOut();
+  //this.router.navigateByUrl('login');
+  //}
+
+  onToggleSideNav(data: SidenavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
+
+}
---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,88 @@
+export const navbarData = [
+  {
+    routeLink: 'home',
+    icon: 'fal fa-home',
+    label: 'INICIO',
+  },
+  {
+    routeLink: 'login',
+    icon: 'far fa-user',
+    label: 'INICIAR SESION',
+  },
+  {
+    routeLink: 'signup',
+    icon: 'far fa-user-plus',
+    label: 'REGISTRATE',
+  },
+];
+
+export const loginData = [
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'INICIO',
+  },
+  {
+    routeLink: 'cart',
+    icon: 'fal fa-home',
+    label: 'CARIITO',
+  },
+  {
+    routeLink: 'my_orders',
+    icon: 'fal fa-home',
+    label: 'ORDENES',
+  },
+  {
+    routeLink: 'wishlist',
+    icon: 'fal fa-home',
+    label: 'FAVORITOS',
+  },
+  {
+    routeLink: 'cart',
+    icon: 'fal fa-home',
+    label: 'SALIR',
+  },
+];
+
+export const adminData = [
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'INICIO',
+  },
+  {
+    routeLink: 'category',
+    icon: 'fal fa-home',
+    label: 'CATEGORIAS',
+  },
+  {
+    routeLink: 'product',
+    icon: 'fal fa-home',
+    label: 'PRODUCTOS',
+  },
+  {
+    routeLink: 'post-coupon',
+    icon: 'fal fa-home',
+    label: 'CREAR CUPON',
+  },
+  {
+    routeLink: 'coupons',
+    icon: 'fal fa-home',
+    label: 'CUPONES',
+  },
+  {
+    routeLink: 'orders',
+    icon: 'fal fa-home',
+    label: 'ORDENES',
+  },
+  {
+    routeLink: 'analytics',
+    icon: 'fal fa-home',
+    label: 'ESTADISTICAS',
+  },
+  {
+    routeLink: 'cart',
+    icon: 'fal fa-home',
+    label: 'SALIR',
+  },
+];
